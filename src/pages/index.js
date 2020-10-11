ef8d9a02a0a721c60359f19e40fecdfa0b41cb7b
ljb <1244659255@qq.com>
2020-10-11 21:02:14 +0800

    tst111

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,10 @@
 import styles from './index.css';
 
 export default function() {
-  var ab = 1;
+  var abddd = 1;
   return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-           test444
-          </a>
-        </li>
-      </ul>
+    <div>
+      111
     </div>
   );
 }
